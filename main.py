98b04c043a06599f11a137f97d594f5c8f83ecf6
NikolaZhal <ryazan62nik@yandex.ru>
2023-12-29 20:21:37 +0300

    change+add

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from PyQt5.QtWidgets import (
     QWidget,
     QMainWindow,
     QTableWidgetItem,
+    QTableWidget,
+    QVBoxLayout,
 )
 from PyQt5 import uic  # Импортируем uic
 from random import randint
@@ -48,7 +50,7 @@ def add_coffee(*data, cur=None):
 @db_opener
 def change_coffee(what="", value="", where="", cur=None):
     """меняет данные о клиенте поле {what} = {value}, id = {where}"""
-    cur.execute(f"""UPDATE clientele SET {what} = '{value}' WHERE id = '{where}'""")
+    cur.execute(f"""UPDATE coffee SET {what} = '{value}' WHERE id = '{where}'""")
 
 
 @db_opener
@@ -69,26 +71,67 @@ def get_from(what="*", where={}, fromdb="coffee", cur=None) -> list:
     return list(result)
 
 
+FIELD = ("id", "name", "roasting", "grains", "taste", "cost", "volume")
+NAMING = (
+    "ID",
+    "название сорта",
+    "степень обжарки",
+    "молотый/в зернах",
+    "описание вкуса",
+    "цена",
+    "объем упаковки",
+)
+
+
+class AddPartOrderInTable(QWidget):
+    def __init__(self, *data):
+        super().__init__()
+        self.main = data[0]
+        self.part = data[-1]
+        self.setGeometry(30, 30, 700, 200)
+        self.tableWidget = QTableWidget(self)
+        self.tableWidget.setGeometry(0, 0, 400, 400)
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(NAMING)
+        self.tableWidget.setRowCount(1)
+        if self.part != -1:
+            for i, elem in enumerate(data[1]):
+                self.tableWidget.setItem(0, i, QTableWidgetItem(elem))
+            self.tableWidget.resizeColumnsToContents()
+            self.tableWidget.itemSelectionChanged.connect(
+                self.tableWidget.resizeColumnsToContents
+            )
+        self.data_send = QPushButton("Ок", self)
+        self.data_send.clicked.connect(self.get_data)
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addWidget(self.tableWidget)
+        self.vbox.addWidget(self.data_send)
+
+    def get_data(self):
+        result = []
+        for col in range(7):
+            result.append(self.tableWidget.item(0, col).text())
+        self.main.get_info(result, self.part)
+
+        # self.main.layout.removeWidget(self.main.secondWidget)
+        # self.main.secondWidget = QTableWidget()
+        # self.main.layout.addWidget(self.main.secondWidget)
+        # print(self.main.data)
+        # return self.main.data
+
+
 class Square1(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)  # Загружаем дизайн
         self.data = get_from()
         self.make_table()
+        self.addCoffee.clicked.connect(self.add)
+        self.changeCoffee.clicked.connect(self.change)
 
     def make_table(self):
         self.tableWidget.setColumnCount(7)
-        self.tableWidget.setHorizontalHeaderLabels(
-            (
-                "ID",
-                "название сорта",
-                "степень обжарки",
-                "молотый/в зернах",
-                "описание вкуса",
-                "цена",
-                "объем упаковки",
-            )
-        )
+        self.tableWidget.setHorizontalHeaderLabels(NAMING )
         self.tableWidget.setRowCount(0)
         for i, row in enumerate(self.data):
             self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
@@ -97,6 +140,34 @@ class Square1(QMainWindow):
         # self.tableWidget.resizeColumnsToContents()
         # self.tableWidget.setColumnWidth(0, 150)
         # self.tableWidget.setColumnWidth(1, 220)
+
+    def showForm(self, data, row=-1):
+        self.vvod = AddPartOrderInTable(self, data, row)
+        self.vvod.show()
+
+    def change(self):
+        y = self.tableWidget.currentRow()
+        data = []
+        for i in range(7):
+            data.append(str(self.tableWidget.item(y, i).text()))
+        self.showForm(data, y)
+
+    def add(self):
+        self.showForm([])
+
+    def get_info(self, data, row):
+        if row == -1:
+            data[0] = int(data[0])
+            self.data.append(data)
+            add_coffee(*data)
+            self.make_table()
+        else:
+            data[0] = int(data[0])
+            self.data[row] = data
+            for i, el in enumerate(FIELD):
+                change_coffee(el, data[i], data[0])
+
+            self.make_table()
 
 
 def except_hook(cls, exception, traceback):
